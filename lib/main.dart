c17f9f92190c192773885182d21bb062dc33c107
Syed Huzaifa Ali <shuzaifa@codebtech.com>
2026-01-05 04:19:55 -0800

    localization setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,35 @@
+// ignore_for_file: unused_local_variable
+
 import 'package:ast_official/app_ui_helpers/app_routes/app_routes.dart';
 import 'package:ast_official/app_ui_helpers/app_routes/route_paths.dart';
+import 'package:ast_official/generated/l10n.dart';
 import 'package:ast_official/services/notification_service/app_notification_service.dart';
 import 'package:ast_official/utils/colors_utils.dart';
 import 'package:ast_official/utils/main_provider/main_provider.dart';
 import 'package:ast_official/utils/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
 import 'package:sizer/sizer.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 void main() async {
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  WidgetsFlutterBinding.ensureInitialized(); // ✅ MUST BE FIRST
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   AppNotificationService notificationService = AppNotificationService();
 
-  //Uncomment it when you create providers
-  runApp(MultiProvider(
-    providers: providersList,
-    child: const MyApp(),
-  ));
-
+  runApp(
+    MultiProvider(
+      providers: providersList,
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -41,6 +50,17 @@ class MyApp extends StatelessWidget {
 
     return Sizer(builder: (context, orientation, deviceType) {
       return MaterialApp(
+        locale: null, // 👈 AUTO detect (English / Spanish)
+        // supportedLocales: S.delegate.supportedLocales,
+        localizationsDelegates: const [
+          S.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          // GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: const [
+          Locale('en', 'US'),
+        ],
         title: 'AST®️ Official App',
         theme: AppTheme.darkTheme,
         debugShowCheckedModeBanner: false,
